d85bb4515c1b555d0a070a9523fa0af665161204
Aishahsatouf <aishasattouf1996@gmail.com>
2021-02-14 22:53:01 +0200

    lab 36

---
+++ store-app/src/app.js
@@ -0,0 +1,21 @@
+
+import './app.scss';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Categories from './components/Categories'
+import Status from './components/Products'
+
+
+function App() {
+  return (
+    <>
+   <Header/>
+   <Categories/>
+   <Status/>
+   <Footer/>
+
+</>
+  );
+}
+
+export default App;
---
+++ store-app/src/components/Categories.js
@@ -0,0 +1,40 @@
+// import { Divider } from '@material-ui/core';
+import React from 'react';
+import { connect } from 'react-redux';
+import { active } from '../reduxStore/categoriesReducer.js';
+import { Link, Typography } from '@material-ui/core'
+
+const activeCategories = props => {
+ 
+    return (
+        <>
+            <div style={{ fontSize: '24px' }}>
+                Browse Our Categories
+        </div>
+            <br />
+            <section>
+                <Typography>
+
+                    {props.activeOne.categories.map((category, idx) => {
+
+                        return <Link key={idx} onClick={() => props.active(category.name)} href="#">{category.display_name.toUpperCase()} | </Link>
+
+                    })}
+                  
+
+
+                </Typography>
+            </section>
+
+
+        </>
+    )
+}
+
+const mapStateToProps = state => ({
+    activeOne: state.categories
+});
+
+const mapDispatchToProps = { active }
+
+export default connect(mapStateToProps, mapDispatchToProps)(activeCategories)
